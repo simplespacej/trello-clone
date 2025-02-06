e35ac5f369aca79c28daae4db68265eb9c7b8d50
simplespacej <my-ptogramming@mail.ru>
2025-02-06 03:25:23 +0300

    Webpack and Appveyor set up

---
+++ src/index.js
@@ -0,0 +1,2 @@
+console.log('Webpack работает!');
+
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    static: './dist',
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
+
